06a6fc1c3b2de09eed4402bce7e31c666828c5d2
rahmaFeki <rahma19fki@gmail.com>
2021-12-21 14:35:49 +0100

    first

--- App/Providers/AppServiceProvider.php
+++ App/Providers/AppServiceProvider.php
@@ -25,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('APP_ENV') != 'local') {
+            URL::forceScheme('https');
+        } 
     }
 }
